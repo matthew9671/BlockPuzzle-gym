cda598f62871b98d9bfe161a10eb4624358458d0
matthew9671 <yixiuz@andrew.cmu.edu>
2018-03-28 17:06:37 -0400

    Tested blocks environment

--- gym_banana/envs/__init__.py
+++ gym_banana/envs/__init__.py
@@ -1,3 +1,5 @@
 from gym_banana.envs.banana_env import BananaEnv
-from gym_banana.envs.pick_and_place import FetchPickAndPlaceEnv
+# from gym_banana.envs.pick_and_place import FetchEnv
+# from gym_banana.envs.pick_and_place import FetchPickAndPlaceEnv
+from gym_banana.envs.fetch_env import FetchBlocksEnv
 from gym_banana.envs.pick_and_place import BlocksEnv
--- gym_banana/envs/pick_and_place.py
+++ gym_banana/envs/pick_and_place.py
@@ -1,8 +1,25 @@
 from gym import utils
 from gym_banana.envs import fetch_env
 
+# class FetchPickAndPlaceEnv(fetch_env.FetchEnv, utils.EzPickle):
+#     def __init__(self, reward_type='sparse'):
+#         initial_qpos = {
+#             'robot0:slide0': 0.405,
+#             'robot0:slide1': 0.48,
+#             'robot0:slide2': 0.0,
+#             'table0:slide0': 1.05,
+#             'table0:slide1': 0.4,
+#             'table0:slide2': 0.0,
+#             'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
+#         }
+#         fetch_env.FetchEnv.__init__(
+#             self, 'fetch/pick_and_place.xml', has_object=True, block_gripper=False, n_substeps=20,
+#             gripper_extra_height=0.2, target_in_the_air=True, target_offset=0.0,
+#             obj_range=0.15, target_range=0.15, distance_threshold=0.05,
+#             initial_qpos=initial_qpos, reward_type=reward_type)
+#         utils.EzPickle.__init__(self)
 
-class FetchPickAndPlaceEnv(fetch_env.FetchEnv, utils.EzPickle):
+class BlocksEnv(fetch_env.FetchBlocksEnv, utils.EzPickle):
     def __init__(self, reward_type='sparse'):
         initial_qpos = {
             'robot0:slide0': 0.405,
@@ -11,28 +28,12 @@ class FetchPickAndPlaceEnv(fetch_env.FetchEnv, utils.EzPickle):
             'table0:slide0': 1.05,
             'table0:slide1': 0.4,
             'table0:slide2': 0.0,
-            'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
+            'object0:joint': [1.25, 0.55, 0.4, 1., 0., 0., 0.],
+            'object1:joint': [1.25, 0.7, 0.4, 1., 0., 0., 0.],
+            # 'object2:joint': [1.25, 0.85, 0.4, 1., 0., 0., 0.],
+            # 'object3:joint': [1.25, 0.1, 0.4, 1., 0., 0., 0.]
         }
-        fetch_env.FetchEnv.__init__(
-            self, 'fetch/pick_and_place.xml', has_object=True, block_gripper=False, n_substeps=20,
-            gripper_extra_height=0.2, target_in_the_air=True, target_offset=0.0,
-            obj_range=0.15, target_range=0.15, distance_threshold=0.05,
-            initial_qpos=initial_qpos, reward_type=reward_type)
-        utils.EzPickle.__init__(self)
-
-class BlocksEnv(fetch_env.FetchEnv, utils.EzPickle):
-    def __init__(self, reward_type='sparse'):
-        initial_qpos = {
-            'robot0:slide0': 0.405,
-            'robot0:slide1': 0.48,
-            'robot0:slide2': 0.0,
-            'table0:slide0': 1.05,
-            'table0:slide1': 0.4,
-            'table0:slide2': 0.0,
-            'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
-            'object1:joint': [1.25, 0.80, 0.4, 1., 0., 0., 0.]
-        }
-        fetch_env.FetchEnv.__init__(
+        fetch_env.FetchBlocksEnv.__init__(
             self, 'fetch/blocks.xml', has_object=True, block_gripper=False, n_substeps=20,
             gripper_extra_height=0.2, target_in_the_air=True, target_offset=0.0,
             obj_range=0.15, target_range=0.15, distance_threshold=0.05,
